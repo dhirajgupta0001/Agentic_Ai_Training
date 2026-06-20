bc36499100d78a6f860e577d788c6b57fdf6a980
Dhiraj Gupta <144586087+dhirajgupta0001@users.noreply.github.com>
2026-06-20 23:43:14 +0530

    Create AgentWithSystemPrompt.py

---
+++ Day 9/AgentWithSystemPrompt.py
@@ -0,0 +1,30 @@
+from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langchain.agents import create_agent
+
+model = ChatOpenAI(
+      api_key="hf_token",
+      base_url="https://router.huggingface.co/v1",
+      model="meta-llama/Llama-3.1-8B-Instruct:novita",
+      temperature=0.7,
+  )
+
+agent = create_agent(
+    model=model,
+    tools=[],
+    system_prompt="""
+    You are a senior Python mentor.
+    Always explain like a teacher.
+    """
+)
+result = agent.invoke(
+    {
+        "messages": [
+            {
+                "role": "user",
+                "content": "Search LangChain"
+            }
+        ]
+    }
+)
+print(result)
