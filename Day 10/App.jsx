f06d1b23002ba0b5a760ab368cc6fe7c10b543ef
Dhiraj Gupta <144586087+dhirajgupta0001@users.noreply.github.com>
2026-06-23 16:39:13 +0530

    Add files via upload

---
+++ Day 10/App.jsx
@@ -0,0 +1,107 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+// import Chatbot from './components/Chatbot'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <section className='flex flex-col justify-center w-full min-h-screen'>
+//         <div className='header w-full h-auto py-3 bg-blue-900'>
+//            <h3 className='text-teal-600 font-bold text-7xl'> Building Agentic AI</h3> 
+//         </div>
+//         <div>
+//           <Chatbot />
+//         </div>
+//       </section>
+//     </>
+//   )
+// }
+
+// export default App
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Header from "./components/header";
+// import Container from "./components/container";
+// import Home from "./pages/Home";
+// import AgenticAI from "./pages/AgenticAI";
+// import Marketplace from "./pages/Marketplace";
+// import Settings from "./pages/Settings";
+
+// function App() {
+//   return (
+//     // <div className="min-h-screen bg-[#030B2F]">
+
+//     //   <Header />
+
+//     //   <Container />
+
+//     // </div>
+//     <BrowserRouter>
+
+//       <div className="min-h-screen bg-[#020817]">
+
+//         <Header />
+//         <Container />
+
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/agentic-ai" element={<AgenticAI />} />
+//           <Route path="/marketplace" element={<Marketplace />} />
+//           <Route path="/settings" element={<Settings />} />
+//         </Routes>
+
+//       </div>
+
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/header";
+import Container from "./components/container"
+import Home from "./pages/Home";
+import AgenticAI from "./pages/AgenticAI";
+import Marketplace from "./pages/Marketplace";
+import Settings from "./pages/Settings";
+import AboutMe from "./pages/AboutMe";
+
+function App() {
+  //<Container />
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen bg-[#020B38]">
+        <Header />
+        
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/agentic-ai" element={<AgenticAI />} />
+
+          <Route
+            path="/marketplace"
+            element={<Marketplace />}
+          />
+
+          <Route
+            path="/settings"
+            element={<Settings />}
+          />
+
+          <Route
+            path="/aboutme"
+            element={<AboutMe />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
