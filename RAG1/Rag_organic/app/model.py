c9f77cf15a9e649d8156bb2ea1cee5a94ebb40a4
Dhiraj Gupta <144586087+dhirajgupta0001@users.noreply.github.com>
2026-07-14 13:40:53 +0530

    Change API key in model.py
    Updated API key for the chat model initialization.

--- RAG1/Rag_organic/app/model.py
+++ RAG1/Rag_organic/app/model.py
@@ -6,6 +6,6 @@ from langchain.chat_models import init_chat_model
 
 model=init_chat_model("gpt-oss:20b-cloud",
     model_provider="openai",
-    api_key="3ebcce2818504f32874cf67012cc6317.C-9s6bKki9tCCDpbzgfWRGNO",
+    api_key="hh",
     base_url="https://ollama.com/v1"
 )
